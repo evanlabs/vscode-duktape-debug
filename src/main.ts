d18e34cdf709ab10cc0ec3e6cc3890a402becb29
Harold Brenes <harito@gmail.com>
2017-01-16 02:15:52 -0400

    Testing in ES5 again. Installed extension does not work...

--- src/main.ts
+++ src/main.ts
@@ -23,12 +23,6 @@ class ProxyDebugSession extends DebugSession
     }
 
     //-----------------------------------------------------------
-    private createInstance():void
-    {
-
-    }
-
-    //-----------------------------------------------------------
     // The 'initialize' request is the first request called by the frontend
     // to interrogate the debug adapter about the features it provides.
     //-----------------------------------------------------------
@@ -58,6 +52,13 @@ class ProxyDebugSession extends DebugSession
             this.sendEvent( new TerminatedEvent( false ) );
         }
 
+        (<any>this._impl).sendResponse = ( response:DebugProtocol.Response ) => {
+            this.sendResponse( response );
+        };
+        (<any>this._impl).sendEvent = ( event:DebugProtocol.Event) => {
+            this.sendEvent( event );
+        };
+
         // Forward event debugger implementation to local
         (<any>this._impl).initializeRequest( response, args );
     }
@@ -69,215 +70,226 @@ class ProxyDebugSession extends DebugSession
                                  format?: string, variables?: any,
                                  dest?: ErrorDestination):void
     {
-        (<any>this._impl).sendErrorResponse( response, codeOrMessage, format, variables, dest );
+        this._impl ? (<any>this._impl).sendErrorResponse( response, codeOrMessage, format, variables, dest ) :
+                        super.sendErrorResponse( response, codeOrMessage, format, variables, dest );
     }
 
     runInTerminalRequest( args:DebugProtocol.RunInTerminalRequestArguments, 
                           timeout:number, 
                           cb:(response:DebugProtocol.RunInTerminalResponse) => void):void
     {
-        super.runInTerminalRequest( args, timeout, cb );
+        this._impl ? (<any>this._impl).runInTerminalRequest( args, timeout, cb ) :
+                        super.runInTerminalRequest( args, timeout, cb );
     }
 
+    /*
     protected dispatchRequest( request:DebugProtocol.Request ):void
     {
-        super.dispatchRequest( request );
+        (<any>this._impl).dispatchRequest( request );
     }
+    */
     
     protected disconnectRequest( response:DebugProtocol.DisconnectResponse,
                                  args:DebugProtocol.DisconnectArguments):void
     {
-        super.disconnectRequest( response, args );
+         this._impl ? (<any>this._impl).disconnectRequest( response, args ) :
+                        super.disconnectRequest( response, args );
     }
     
     protected launchRequest( response:DebugProtocol.LaunchResponse,
                              args:DebugProtocol.LaunchRequestArguments ):void
     {
-        super.launchRequest( response, args );
+        (<any>this._impl).launchRequest( response, args );
     }
     
     protected attachRequest( response:DebugProtocol.AttachResponse, 
                              args:DebugProtocol.AttachRequestArguments ):void
     {
-        super.attachRequest( response, args );
+        (<any>this._impl).attachRequest( response, args );
     }
     
     protected restartRequest( response:DebugProtocol.RestartResponse,
                               args:DebugProtocol.RestartArguments ):void
     {
-        super.restartRequest( response, args );
+        (<any>this._impl).restartRequest( response, args );
     }
     
     protected setBreakPointsRequest( response:DebugProtocol.SetBreakpointsResponse,
                                      args:DebugProtocol.SetBreakpointsArguments ):void
     {
-        super.setBreakPointsRequest( response, args );
+        (<any>this._impl).setBreakPointsRequest( response, args );
     }
     
     protected setFunctionBreakPointsRequest( response:DebugProtocol.SetFunctionBreakpointsResponse,
                                              args:DebugProtocol.SetFunctionBreakpointsArguments ):void
     {
-        super.setFunctionBreakPointsRequest( response, args );
+        (<any>this._impl).setFunctionBreakPointsRequest( response, args );
     }
     
     protected setExceptionBreakPointsRequest( response:DebugProtocol.SetExceptionBreakpointsResponse, 
                                               args:DebugProtocol.SetExceptionBreakpointsArguments ):void
     {
-        super.setExceptionBreakPointsRequest( response, args );
+        (<any>this._impl).setExceptionBreakPointsRequest( response, args );
     }
     
     protected configurationDoneRequest( response:DebugProtocol.ConfigurationDoneResponse, 
                                         args:DebugProtocol.ConfigurationDoneArguments ):void
     {
-        super.configurationDoneRequest( response, args );
+        (<any>this._impl).configurationDoneRequest( response, args );
     }
     
     protected continueRequest( response:DebugProtocol.ContinueResponse, 
                                args:DebugProtocol.ContinueArguments ):void
     {
-        super.continueRequest( response, args );
+        (<any>this._impl).continueRequest( response, args );
     }
     
     protected nextRequest( response:DebugProtocol.NextResponse,
                            args:DebugProtocol.NextArguments ):void
     {
-        super.nextRequest( response, args );
+        (<any>this._impl).nextRequest( response, args );
     }
     
     protected stepInRequest( response:DebugProtocol.StepInResponse,
                              args:DebugProtocol.StepInArguments ):void
     {
-        super.stepInRequest( response, args );
+        (<any>this._impl).stepInRequest( response, args );
     }
     
     protected stepOutRequest( response:DebugProtocol.StepOutResponse,
                               args:DebugProtocol.StepOutArguments ):void
     {
-        super.stepOutRequest( response, args );
+        (<any>this._impl).stepOutRequest( response, args );
     }
     
     protected stepBackRequest( response:DebugProtocol.StepBackResponse, 
                                args:DebugProtocol.StepBackArguments ):void
     {
-        super.stepBackRequest( response, args );
+        (<any>this._impl).stepBackRequest( response, args );
     }
     
     protected reverseContinueRequest( response:DebugProtocol.ReverseContinueResponse,
                                       args:DebugProtocol.ReverseContinueArguments ):void
     {
-        super.reverseContinueRequest( response, args );
+        (<any>this._impl).reverseContinueRequest( response, args );
     }
     
     protected restartFrameRequest( response:DebugProtocol.RestartFrameResponse, 
                                    args:DebugProtocol.RestartFrameArguments ):void
     {
-        super.restartFrameRequest( response, args );
+        (<any>this._impl).restartFrameRequest( response, args );
     }
     
     protected gotoRequest( response:DebugProtocol.GotoResponse,
                            args:DebugProtocol.GotoArguments ):void
     {
-        super.gotoRequest( response, args );
+        (<any>this._impl).gotoRequest( response, args );
     }
     
     protected pauseRequest( response:DebugProtocol.PauseResponse,
                             args:DebugProtocol.PauseArguments ):void
     {
-        super.pauseRequest( response, args );
+        (<any>this._impl).pauseRequest( response, args );
     }
     
     protected sourceRequest( response:DebugProtocol.SourceResponse,
                              args:DebugProtocol.SourceArguments ):void
     {
-        super.sourceRequest( response, args );
+        (<any>this._impl).sourceRequest( response, args );
     }
     
     protected threadsRequest( response:DebugProtocol.ThreadsResponse ):void
     {
-        super.threadsRequest( response );
+        (<any>this._impl).threadsRequest( response );
     }
     
     protected stackTraceRequest( response:DebugProtocol.StackTraceResponse, 
                                  args:DebugProtocol.StackTraceArguments ):void
     {
-        super.stackTraceRequest( response, args );
+        (<any>this._impl).stackTraceRequest( response, args );
     }
     
     protected scopesRequest( response:DebugProtocol.ScopesResponse, 
                              args:DebugProtocol.ScopesArguments ):void
     {
-        super.scopesRequest( response, args );
+        (<any>this._impl).scopesRequest( response, args );
     }
     
     protected variablesRequest( response:DebugProtocol.VariablesResponse, 
                                 args:DebugProtocol.VariablesArguments ):void
     {
-        super.variablesRequest( response, args );
+        (<any>this._impl).variablesRequest( response, args );
     }
     
     protected setVariableRequest( response:DebugProtocol.SetVariableResponse,
                                   args:DebugProtocol.SetVariableArguments ):void
     {
-        super.setVariableRequest( response, args );
+        (<any>this._impl).setVariableRequest( response, args );
     }
     
     protected evaluateRequest( response:DebugProtocol.EvaluateResponse,
                                args:DebugProtocol.EvaluateArguments ):void
     {
-        super.evaluateRequest( response, args );
+        (<any>this._impl).evaluateRequest( response, args );
     }
     
     protected stepInTargetsRequest( response:DebugProtocol.StepInTargetsResponse, 
                                     args:DebugProtocol.StepInTargetsArguments): void
     {
-        super.stepInTargetsRequest( response, args );
+        (<any>this._impl).stepInTargetsRequest( response, args );
     }
     
     protected gotoTargetsRequest( response:DebugProtocol.GotoTargetsResponse, 
                                   args:DebugProtocol.GotoTargetsArguments ):void
     {
-        super.gotoTargetsRequest( response, args );
+        (<any>this._impl).gotoTargetsRequest( response, args );
     }
     
     protected completionsRequest( response:DebugProtocol.CompletionsResponse, 
                                   args:DebugProtocol.CompletionsArguments ):void
     {
-        super.completionsRequest( response, args );
+        (<any>this._impl).completionsRequest( response, args );
     }
 
     protected customRequest( command:string, response:DebugProtocol.Response, args:any ):void
     {
-        super.customRequest( command, response, args );
+        (<any>this._impl).customRequest( command, response, args );
     }
     
     protected convertClientLineToDebugger( line:number ):number
     {
-        return super.convertClientLineToDebugger( line );
+        return this._impl ? (<any>this._impl).convertClientLineToDebugger( line ) :
+                            super.convertClientLineToDebugger( line );
     }
     
     protected convertDebuggerLineToClient( line:number ):number
     {
-         return super.convertDebuggerLineToClient( line );
+         return this._impl ? (<any>this._impl).convertDebuggerLineToClient( line ) :
+                             super.convertDebuggerLineToClient( line );
     }
     
     protected convertClientColumnToDebugger( column:number ):number
     {
-        return super.convertClientColumnToDebugger( column );
+        return this._impl ? (<any>this._impl).convertClientColumnToDebugger( column ) :
+                            super.convertClientColumnToDebugger( column );
     }
     
     protected convertDebuggerColumnToClient( column:number ):number
     {
-        return super.convertDebuggerColumnToClient( column );
+        return this._impl ? (<any>this._impl).convertDebuggerColumnToClient( column ) : 
+                            super.convertDebuggerColumnToClient( column );
     }
     
     protected convertClientPathToDebugger( clientPath:string ):string
     {
-        return super.convertClientPathToDebugger( clientPath );
+        return this._impl ? (<any>this._impl).convertClientPathToDebugger( clientPath ) :
+                            super.convertClientPathToDebugger( clientPath );
     }
     
     protected convertDebuggerPathToClient( debuggerPath:string ):string
     {
-        return super.convertDebuggerPathToClient( debuggerPath );
+        return this._impl ? (<any>this._impl).convertDebuggerPathToClient( debuggerPath ) :
+                            super.convertDebuggerPathToClient( debuggerPath );
     }
 }
 
